1f1df01a51c76344652a88bc08bd60a6649aa721
aidan <aidanguarniere@gmail.com>
2020-10-27 17:15:25 -0700

    removed auto-refresh event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 //task button
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 //generate new task
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+
   }
 
     //callback through click event on button
-    buttonEl.addEventListener("click", createTaskHandler);
+    formEl.addEventListener("submit", createTaskHandler);
 
