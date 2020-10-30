64c386a33ed5d91fb8544019811a9f253ba68e46
aidan <aidanguarniere@gmail.com>
2020-10-29 17:24:07 -0700

    added edit button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,10 +34,42 @@ var taskFormHandler = function(event) {
         type: taskTypeInput
     };
 
+    var isEdit = formEl.hasAttribute("data-task-id");
+    
+    // has data attribute, so get task id and call function to complete edit process
+    if (isEdit) {
+        var taskId = formEl.getAttribute("data-task-id");
+        completeEditTask(taskNameInput, taskTypeInput, taskId);
+        } 
+    // no data attribute, so create object as normal and pass to createTaskEl function
+    else {
+        var taskDataObj = {
+            name: taskNameInput,
+            type: taskTypeInput
+            };
+
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
-
+        }
 }
+
+// completeEditTask
+var completeEditTask = function(taskName, taskType, taskId) {
+
+    // find the matching task list item
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // set new values
+    taskSelected.querySelector("h3.task-name").textContent = taskName;
+    taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    alert("Task Updated!");
+
+    // close edit mode and restore Add Task button
+    formEl.removeAttribute("data-task-id");
+    document.querySelector("#save-task").textContent = "Add Task";
+
+    };
 
 // create task function
 var createTaskEl = function(taskDataObj) {
